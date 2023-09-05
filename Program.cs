52d0ecf80ca51a900ccab548476466dc60907d7e
Rusyit <r.hayrullin06@mail.ru>
2023-09-05 20:47:47 +0300

    AsA
    asS

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Program {
+
+class Program {
+
+    static void Main() {
+        Console.WriteLine("МирТрудМай");
+        Console.WriteLine("Мир");
+        Console.WriteLine("  Труд");
+        Console.WriteLine("      Май");
+        }
+    
+    }
+
+}
