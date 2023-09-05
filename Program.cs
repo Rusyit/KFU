9c07564d9cc7ad33787f87a8cfa6aa04628656ee
Rusyit <r.hayrullin06@mail.ru>
2023-09-05 23:18:40 +0300

    dz

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace prog02092023
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("МирТрудМай");
+            Console.WriteLine("Мир");
+            Console.WriteLine("  Труд");
+            Console.WriteLine("     Май");
+            int num1 = 0, num2 = 0;
+            num1 = Convert.ToInt32(Console.ReadLine());
+            num2 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(num2 + "," + num1);
+        }
+        
+    }
+    
+}
